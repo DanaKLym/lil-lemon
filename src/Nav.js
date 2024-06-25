853b8ceb9664706dd6262526e8dc2c79d7d1dad0
DanaKLym <bohdanaklymenko@gmail.com>
2024-06-25 18:43:48 +0300

    added meta tags and OG protocol tags for better SEO

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,26 @@
 function Nav() {
   return (
     <>
-      <div>This is a Navbar</div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Reservations</a>
+        </li>
+        <li>
+          <a href="#">Order online</a>
+        </li>
+        <li>
+          <a href="#">Log in</a>
+        </li>
+      </ul>
     </>
   );
 }
