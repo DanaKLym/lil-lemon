c6bb7da6de0258c6c2bbb42cd8e656e214455e98
DanaKLym <bohdanaklymenko@gmail.com>
2024-07-04 00:20:14 +0300

    set up layout for main section in Home component and About component

--- src/About.js
+++ src/About.js
@@ -6,6 +6,15 @@ function About() {
     <>
       <Nav />
       <h2>This a About section</h2>
+      <p>Short descripton: chefs mentioned</p>
+      <div>
+        <h2>Testimonials: component</h2>
+        <p>Testimonials cards: photos, names, rates, actual feedback</p>
+      </div>
+      <div>
+        <h2>Location: component</h2>
+        <p>iframe and address</p>
+      </div>
       <Footer />
     </>
   );
